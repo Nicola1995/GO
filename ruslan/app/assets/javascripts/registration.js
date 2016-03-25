7e2280bb0f799739dafaeb5f3f985607c2d97f86
Nicola1995 <umniknicola@yandex.ru>
2016-03-25 23:27:07 +0300

    registration v0.0

---
+++ ruslan/app/assets/javascripts/registration.js
@@ -0,0 +1,13 @@
+var pids['player1', 'player2', 'player3', 'player4', 'player5', 'player6']
+var pNum;
+
+function regOnLoad (){
+	alert("onLoad");	
+	pNum = 3;
+	for (var i = pNum; i < 6; i++)
+		document.getElementById(pids[i]).hidden = true;
+}
+
+function playerAdd () {
+	alert('works');
+}
